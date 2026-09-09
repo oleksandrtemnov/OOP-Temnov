1e0f9ed26e2a0251b62c5d767bd262aebe59de1d
oleksandrtemnov <temnov.o.e_kn24@rcit.ukr.education>
2026-09-09 22:25:57 +0300

    Initial commit

---
+++ lab1v5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OOPLab1
+{
+    public class City
+    {
+        private string name;
+        private string country;
+
+        public int Population { get; set; }
+
+        public City(string name, string country, int population)
+        {
+            this.name = name;
+            this.country = country;
+            Population = population;
+        }
+
+        ~City()
+        {
+            Console.WriteLine($"[Деструктор]: Об'єкт міста {name} видалено з пам'яті.");
+        }
+
+        public string GetInfo()
+        {
+            return $"Місто: {name} | Країна: {country} | Населення: {Population:N0} осіб";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            City city1 = new City("Київ", "Україна", 2950000);
+            City city2 = new City("Рівне", "Україна", 243000);
+            City city3 = new City("Токіо", "Японія", 13960000);
+
+            Console.WriteLine("=== Інформація про міста ===");
+            Console.WriteLine(city1.GetInfo());
+            Console.WriteLine(city2.GetInfo());
+            Console.WriteLine(city3.GetInfo());
+        }
+    }
+}
